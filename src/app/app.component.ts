6b3b27eddec55e2413ae6606c2b87694fecc4f6d
Mohammad Reza Taghimomeni <mrmtm95@outlook.com>
2024-03-28 09:53:57 +0330

    work on calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButton } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButton, MatDividerModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatButton } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [MatButton, MatDividerModule],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+  styleUrl: './calendar.component.scss',
 })
-export class CalendarComponent {
-
-}
+export class CalendarComponent {}
