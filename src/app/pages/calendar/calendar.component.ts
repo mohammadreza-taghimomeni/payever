022a7c5da72eed5c2909b1dbb697a670153d9dd1
Mohammad Reza Taghimomeni <mrmtm95@outlook.com>
2024-03-28 15:29:46 +0330

    complete show date time

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -96,4 +96,10 @@ export class CalendarComponent {
       }
     });
   }
+
+  addHours(date: Date, hours: number) {
+    const hoursToAdd = hours * 60 * 60 * 1000;
+    date.setTime(date.getTime() + hoursToAdd);
+    return date;
+  }
 }
